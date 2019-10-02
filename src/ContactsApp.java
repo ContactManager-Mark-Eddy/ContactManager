3ca25932558993cf83aaa97b6eef33adb759ea3e
Mark Strange <strange.mark88@gmail.com>
2019-10-02 16:33:13 -0500

    add view all

--- src/ContactsApp.java
+++ src/ContactsApp.java
@@ -14,37 +14,76 @@ import java.util.List;
 public class ContactsApp {
 
     public static void main(String[] args) {
-//        displayMethod();
+        displayMethod();
 //
 //        Contacts contact = new Contacts("Micheal", "Debrah", "21002038503");
-        Path addContact = Paths.get("contacts.txt");
+//        Path addContact = Paths.get("contacts.txt");
 
-        String test1 = addContact.toString();
-
-        System.out.println(readFileIntoList(test1));
+//        String test1 = addContact.toString();
+//
+//        System.out.println(readFileIntoList(test1));
 
     }
 
     public static void displayMethod() {
+        Input input = new Input();
+
         System.out.println("1. View contacts.\n" +
                 "2. Add a new contact.\n" +
                 "3. Search a contact by name.\n" +
                 "4. Delete an existing contact.\n" +
-                "5. Exit.\n" +
-                "Enter an option (1, 2, 3, 4 or 5):");
-    }
+                "5. Exit.\n");
 
-    public static List<String> readFileIntoList(String file) {
-        List<String> lines = Collections.emptyList();
-        try {
-            lines = Files.readAllLines(Paths.get(file), StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            e.printStackTrace();
+        int choice = input.getInt("Enter an option (1, 2, 3, 4 or 5):");
+
+        if(choice == 1){
+//            System.out.println(displayAll());
+            displayAll();
         }
 
-        System.out.println(lines);
-        return lines;
-
     }
+
+    public static void displayAll(){
+
+        Path testFile = Paths.get("contacts.txt");
+        List<String> lines = new ArrayList<>();
+        try{
+            lines = Files.readAllLines(testFile);
+        } catch (Exception e){
+            e.printStackTrace();
+            System.out.println("Tha did not work");
+        }
+//        System.out.println(lines.get(0));
+
+        System.out.println("Name | Phone number\n" +
+                "---------------\n");
+
+        String[] parsedString;
+        for (String contact: lines) {
+            parsedString = contact.split(";");
+
+        System.out.println(parsedString[0] + " " + parsedString[1] + " | " + parsedString[2] + "\n");
+
+
+        }
+
+
+
+//        return lines;
+    }
+
+
+//    public static List<String> readFileIntoList(String file) {
+//        List<String> lines = Collections.emptyList();
+//        try {
+//            lines = Files.readAllLines(Paths.get(file), StandardCharsets.UTF_8);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+////        System.out.println(lines);
+//        return lines;
+//
+//    }
 
 }
